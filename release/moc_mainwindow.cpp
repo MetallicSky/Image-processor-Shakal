9c24092ccee32e103360ae0f6d0f2629ad09a946
Dmitriy <46136189+Metlaski@users.noreply.github.com>
2022-11-22 15:35:46 +0300

    Add files via upload

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,150 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Shakal_2/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[11];
+    char stringdata0[252];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 28), // "on_pushButton_select_clicked"
+QT_MOC_LITERAL(2, 40, 0), // ""
+QT_MOC_LITERAL(3, 41, 35), // "on_verticalSlider_zoom_valueC..."
+QT_MOC_LITERAL(4, 77, 5), // "value"
+QT_MOC_LITERAL(5, 83, 27), // "on_checkBox_bw_stateChanged"
+QT_MOC_LITERAL(6, 111, 4), // "arg1"
+QT_MOC_LITERAL(7, 116, 38), // "on_horizontalSlider_level_val..."
+QT_MOC_LITERAL(8, 155, 40), // "on_horizontalSlider_level_sli..."
+QT_MOC_LITERAL(9, 196, 26), // "on_pushButton_undo_clicked"
+QT_MOC_LITERAL(10, 223, 28) // "on_spinBox_size_valueChanged"
+
+    },
+    "MainWindow\0on_pushButton_select_clicked\0"
+    "\0on_verticalSlider_zoom_valueChanged\0"
+    "value\0on_checkBox_bw_stateChanged\0"
+    "arg1\0on_horizontalSlider_level_valueChanged\0"
+    "on_horizontalSlider_level_sliderReleased\0"
+    "on_pushButton_undo_clicked\0"
+    "on_spinBox_size_valueChanged"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    1,   50,    2, 0x08 /* Private */,
+       5,    1,   53,    2, 0x08 /* Private */,
+       7,    1,   56,    2, 0x08 /* Private */,
+       8,    0,   59,    2, 0x08 /* Private */,
+       9,    0,   60,    2, 0x08 /* Private */,
+      10,    1,   61,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    6,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_select_clicked(); break;
+        case 1: _t->on_verticalSlider_zoom_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->on_checkBox_bw_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_horizontalSlider_level_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->on_horizontalSlider_level_sliderReleased(); break;
+        case 5: _t->on_pushButton_undo_clicked(); break;
+        case 6: _t->on_spinBox_size_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
